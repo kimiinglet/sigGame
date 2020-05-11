a5291d7b9cf1321c47fcf26d8aae931830f11bdf
kimiinglet <kinglet91391@gmail.com>
2020-05-10 20:15:09 -0500

    jquery psuedocode

---
+++ script.js
@@ -0,0 +1,36 @@
+//PsuedoCode Practice
+//Create element by tagName;
+var h1Tag = document.createElement("h1");
+var h2Tag = document.createElement("h2");
+var h3Tag = document.createElement("h3");
+var listType = document.createElement("ul");
+var list1 = document.createElement("li");
+var list2 = document.createElement("li");
+var list3 = doucment.createElement("li");
+var button = doucment.createElement("button");
+
+document.getElementsByTagName
+
+//tag.textContent;
+h1Tag.textContent = "Welcome to Date Night";
+h2Tag.textContent = "Quiz/ Know each other";
+h3Tag.textContent = "Game time!";
+listType.textContent = "Pick Your game:";
+list1.textContent = "Remmeber the early days?";
+list2.textContent = "Favorites";
+list3.textContent = "Future Goals";
+button.textContent= "Lego";
+
+//appendChild
+document.body.appendChild(h1Tag);
+document.body.appendChild(h2Tag);
+doucment.body.appendChild(h3Tag);
+document.body.appendChild(listType);
+document.body.appendChild(list1);
+document.body.appendChild(list2);
+doucment.body.appendChild(list3);
+document.body.appendChild(button);
+
+//setAttribute
+
+
